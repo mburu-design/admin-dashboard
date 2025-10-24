d36360094e5c01de77d9859bf3df2c95ff7645bd
mburu-design <mburukennedy67@gmail.com>
2025-10-24 12:30:17 +0300

    show all payments , fixed

--- leads.php
+++ leads.php
@@ -8,7 +8,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit(0);
 }
 
-$BASE_URL = 'https://crmbackend.myaccuratebook.com/api/admin';
+// $BASE_URL = 'https://crmbackend.myaccuratebook.com/api/admin';
+$BASE_URL = 'http://localhost:7000/api/admin';
+
 $AUTH_TOKEN = 'zWWq5BWO+anUMgWtimvvCguXwU=wAMnzI6grv9WkCFsIdkBydGV4SDZQQHNz';
 
 function makeRequest($endpoint, $method = 'GET', $data = null, $token = null) {
